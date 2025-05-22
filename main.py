fe35a84c7e71c416500467229924d5c970646c40
sirbread <gverma0824@outlook.com>
2025-05-21 22:13:20 -0600

    post submit requests done (turns out you can't watch yt videos and code at the same time)

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from game_state import add_submission, calculate_scores, current_round, reset_round
 
-thing = FastAPI()
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-@thing.get("/")
+mst = ZoneInfo("America/Denver")
 
-def dothing():
-    return {"message": "chat is this real"}
+app = FastAPI()
+
+class Submission(BaseModel):
+    user_id: int
+    number_selected: int
+
+@app.post("/submit/")
+def submit(data: Submission):
+    now = datetime.now(mst)
+    if now > current_round["end_time"]:
+        reset_round()
+    add_submission(data.user_id, data.number_selected)
+    return {"status": "submitted"}
+
+@app.get("/scores/")
+def get_scores():
+    return calculate_scores()
+@app.get("/round/")
+def get_round_info():
+    return {
+        "round_id": current_round["id"],
+        "time_left_minutes": max(0,int((current_round["end_time"]-datetime.now(mst)).total_seconds()//60))
+    }
